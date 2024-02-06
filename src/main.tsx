fb4d027e5bae8457429af14a2ace15b596a1d5fc
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-02-06 09:26:39 +0000

    clean code

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import {
-  Outlet,
-  // RouterProvider,
-  Link,
-  createRouter,
-  createRoute,
-  createRootRoute,
-} from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
 import CustomRouterProvider from "./routes/RouterProvider";
 import router from "./routes/routes";
 
