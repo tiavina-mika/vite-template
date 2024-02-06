1342738eef577d0d3cfe3834154c27b0eb607a90
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-02-06 13:21:23 +0000

    add is auth

--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -2,9 +2,10 @@ import About from "../pages/About";
 import Layout from "../pages/Layout";
 import Home from "../pages/Home";
 
-import { createRoute } from "@tanstack/react-router";
+import { createRoute, redirect } from "@tanstack/react-router";
 import { appLayout } from "./routes";
 import articleRoutes, { articlesLayout } from "./article.routes";
+import { isAuth } from "../constants";
 
 /**
  * add id to pathless route (sub layouts)
@@ -14,6 +15,11 @@ export const privateLayout = createRoute({
   id: "private",
   getParentRoute: () => appLayout,
   component: Layout,
+  loader: (): void => {
+    if (!isAuth) {
+      redirect({ to: "/login", throw: true });
+    }
+  },
 });
 
 const HomeRoute = createRoute({
