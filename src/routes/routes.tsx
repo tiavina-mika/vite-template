a147820c66bdc4c09dd7a0497453231eab6d1154
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-02-06 12:54:22 +0000

    clean code

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,9 @@
-import {
-  createRouter,
-  createRootRoute,
-  Outlet,
-  // createRoute,
-} from "@tanstack/react-router";
 import React from "react";
-// import Layout from "../pages/Layout";
+import { createRouter, createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
 import publicRoutes from "./public.routes";
 import privateRoutes from "./private.routes";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const appLayout = createRootRoute({
   component: () => (
@@ -18,55 +13,6 @@ export const appLayout = createRootRoute({
     </>
   ),
 });
-
-// const privateLayout = createRoute({
-//   id: "dashboard",
-//   getParentRoute: () => appLayout,
-//   component: () => (
-//     <div>
-//       <h3>Dashboard</h3>
-//       <Outlet />
-//     </div>
-//   ),
-//   // path: "/",
-// });
-
-// const publicLayout = createRoute({
-//   id: "public",
-//   getParentRoute: () => appLayout,
-//   component: () => (
-//     <div>
-//       <h3>Public</h3>
-//       <Outlet />
-//     </div>
-//   ),
-// });
-
-// const HomeRoute = createRoute({
-//   getParentRoute: () => privateLayout,
-//   component: () => (
-//     <div>
-//       <div>Home</div>
-//     </div>
-//   ),
-//   path: "/",
-// });
-
-// const LoginRoute = createRoute({
-//   getParentRoute: () => publicLayout,
-//   component: () => (
-//     <div>
-//       <div>Login</div>
-//     </div>
-//   ),
-//   path: "/login",
-// });
-
-// const routeTree = appLayout.addChildren([
-//   privateLayout.addChildren([HomeRoute]),
-//   // HomeRoute,
-//   publicLayout.addChildren([LoginRoute]),
-// ]);
 
 const routeTree = appLayout.addChildren([publicRoutes, privateRoutes]);
 
