09c2d9035a5837049e8d5e907da0b9558e1a414b
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-02-07 07:54:12 +0000

    add article

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -4,30 +4,27 @@ import { Outlet, Link } from "@tanstack/react-router";
 const DashboardLayout = () => {
   return (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" activeProps={{ className: `font-bold` }}>
-          Home
-        </Link>{" "}
-        <Link to="/about" activeProps={{ className: `font-bold` }}>
-          About
-        </Link>
-        <Link to="/articles" activeProps={{ className: `font-bold` }}>
-          Articles
-        </Link>
-        <Link
-          to="/articles/$id"
-          params={{ id: "3" }}
-          activeProps={{ className: `font-bold` }}
-        >
-          Article
-        </Link>
-        <Link to="/profile" activeProps={{ className: `font-bold` }}>
-          Profile
-        </Link>
-        <Link to="/logout" activeProps={{ className: `font-bold` }}>
-          Logout
-        </Link>
-      </div>
+      <nav className="p-2 flex gap-2 justify-between">
+        <div className="flex gap-3">
+          <Link to="/" activeProps={{ className: `font-bold` }}>
+            Home
+          </Link>{" "}
+          <Link to="/about" activeProps={{ className: `font-bold` }}>
+            About
+          </Link>
+          <Link to="/articles" activeProps={{ className: `font-bold` }}>
+            Articles
+          </Link>
+        </div>
+        <div className="flex gap-3">
+          <Link to="/profile" activeProps={{ className: `font-bold` }}>
+            Profile
+          </Link>
+          <Link to="/logout" activeProps={{ className: `font-bold` }}>
+            Logout
+          </Link>
+        </div>
+      </nav>
       <hr />
       <div className="p-2">
         <Outlet />
--- src/pages/articles/Article.tsx
+++ src/pages/articles/Article.tsx
@@ -11,15 +11,15 @@ const Article = () => {
     <div className="space-y-3">
       <div className="space-x-2">
         <span className="text-grey-500">Id:</span>
-        <span className="text-medium">{id}</span>
+        <span className="font-medium">{id}</span>
       </div>
       <div className="space-x-2">
         <span className="text-grey-500">Title:</span>
-        <span className="text-medium">{article.title}</span>
+        <span className="font-medium">{article.title}</span>
       </div>
       <div className="flex flex-col space-y-1">
         <span className="text-grey-500">Content:</span>
-        <p className="text-medium">{article.content}</p>
+        <p className="font-medium">{article.content}</p>
       </div>
     </div>
   );
