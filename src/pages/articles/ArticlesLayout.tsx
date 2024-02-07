6b323b267b52f9363e0fb57afa05ae8d4c592b18
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-02-07 11:30:02 +0000

    index.ts

--- src/pages/articles/ArticlesLayout.tsx
+++ src/pages/articles/ArticlesLayout.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import {
-  Outlet,
-} from "@tanstack/react-router";
+import { Outlet, Link } from "@tanstack/react-router";
 
 const ArticlesLayout = () => {
-    return (
-        <div>
-          <div>Articles layout</div>
-          <Outlet />
-        </div>
-      )
-}
+  return (
+    <div>
+      <div>Articles layout</div>
+      <Outlet />
+    </div>
+  );
+};
 
 export default ArticlesLayout;
